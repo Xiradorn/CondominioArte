ecc65d96be20828b0b57e3abc22df14f90fb4a59
Xiradorn <xiradorn@gmail.com>
2014-08-27 17:20:16 +0200

    update contact structure file: contatti.html, js/contact_me.js and mail/contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,10 +17,10 @@ $message = $_POST['message'];
 	
 // Create the email and send the message
 $to = 'xiradorn@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Contact Form da CondominioArte:  $name";
+$email_subject = "Contact Form da Condominio dell&#39;Arte: $name";
 /*$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";*/
 $email_body = "Hai ricevuto un nuovo messaggio dalla form di contatto del sito.\n\n"."Questo è il testo del messaggio:\n\nNome: $name\n\nEmail: $email_address\n\nMessagio:\n$message";
-$headers = "Da: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "Da: noreply@condominiodellarte.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Rispondi a: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
